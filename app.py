757af1d5c8910d0247c19f4c0b94eef78ae9d19c
Connor Kelleher <connor.r.kelleher@gmail.com>
2022-09-28 09:51:28 -0400

    simple flask versionn

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from pystrokes import getStrokeSvgs, getStrokeMap
 from flask import Flask, render_template, request
 app = Flask(__name__)
  
-#template = 'template/index.html'
  
 file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
@@ -12,29 +11,20 @@ env.trim_blocks = True
 env.lstrip_blocks = True
 env.rstrip_blocks = True
 template = env.get_template('index.html')
-#filename = os.path.join(root, 'html', 'index.html')
 filename = 'index.html'
 
-pngs = [
-  "tmp-strokes/stroke_00.png",
-  "tmp-strokes/stroke_01.png",
-  "tmp-strokes/stroke_02.png",
-  "tmp-strokes/stroke_03.png",
-  "tmp-strokes/stroke_04.png",
-  "tmp-strokes/stroke_05.png",
-  "tmp-strokes/stroke_06.png"
-]
-#ch = '裤'
-ch = '姐'
+@app.route("/")
+def form():
+  return render_template('form.html')
 
-svgs = getStrokeSvgs(getStrokeMap(ch))
-print(svgs)
-
-with open(filename, 'w') as fh:
-    fh.write(template.render(
-#       stroke_pngs = pngs,
-        stroke_svgs = svgs,
-        width = 400,
-        height = 400,
-    ))
-
+@app.route("/data/", methods = ['POST', 'GET'])
+def data():
+  if request.method == 'GET':
+    return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+  if request.method == 'POST':
+    form_data = request.form
+    ch = form_data['character']
+    svgs = getStrokeSvgs(getStrokeMap(ch))
+    return template.render(stroke_svgs = svgs, width = 400, height = 400)
+ 
+app.run(host='localhost', port=5000)
